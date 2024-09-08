40b78798a9ceaf6f1f15ac7495d7e8c959d68ee8
Syahdan Nurmansyah <syahdan.nurmansyah@gmail.com>
2024-09-08 20:53:14 +0700

    openMusic V2 implemented

--- src/server.js
+++ src/server.js
@@ -1,26 +1,56 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const ClientError = require('./exceptions/ClientError');
 
-// Album
+// Albums
 const albums = require('./api/albums');
 const AlbumsValidator = require('./validators/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 
-// Song
+// Songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validators/songs');
 
-const init = async () => {
+// Users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validators/users');
 
-    const albumService = new AlbumsService();
+// Authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const AuthenticationsValidator = require('./validators/authentications');
+const TokenManager = require('./tokenize/TokenManager');
+
+// Playlists
+const playlists = require('./api/playlists');
+const PlaylistsService = require('./services/postgres/PlaylistsService');
+const PlaylistsValidator = require('./validators/playlists');
+
+// SongToPlaylist
+const playlistSongs = require('./api/playlistSongs');
+const PlaylistSongsService = require('./services/postgres/PlaylistSongsService');
+const PlaylistSongsValidator = require('./validators/playlistSongs');
+
+const init = async () => {
+    
     const songsService = new SongsService();
+    const albumService = new AlbumsService();
+    const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
+    const playlistsService = new PlaylistsService();
+    const playlistSongsService = new PlaylistSongsService();
 
     const server = Hapi.server ({
         port: process.env.PORT,
         host: process.env.HOST,
+        debug: {
+            request: ['error'],
+        },
+        
         routes: {
             cors: {
                 origin: ['*'],
@@ -28,20 +58,77 @@ const init = async () => {
         },
     });
 
+    // Register plugin eksternal
+
+    await server.register ([
+        {
+            plugin: Jwt,
+        }
+    ]);
+
+    server.auth.strategy('openmusic_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
+    })
+
     await server.register ([
         {
             plugin: albums,
             options: {
-                albumsService: albumService,
+                service: albumService,
                 songsService: songsService,
-                albumsValidator: AlbumsValidator,
+                validator: AlbumsValidator,
             },
         },
         {
             plugin: songs,
             options: {
-                songsService: songsService,
-                songsValidator: SongsValidator,
+                service: songsService,
+                validator: SongsValidator,
+            }
+        },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
+            }
+        },
+        {
+            plugin: playlists,
+            options: {
+                service: playlistsService,
+                validator: PlaylistsValidator,
+            }
+        },
+        {
+            plugin: playlistSongs,
+            options: {
+                service: playlistSongsService,
+                playlistService: playlistsService,
+                songService: songsService,
+                validator: PlaylistSongsValidator,
             }
         },
     ]);
