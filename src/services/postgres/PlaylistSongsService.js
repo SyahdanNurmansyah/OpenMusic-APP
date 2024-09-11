ec0223c25b53e193b759bbe82c6a75bd06070b1d
Syahdan Nurmansyah <syahdan.nurmansyah@gmail.com>
2024-09-11 19:58:10 +0700

    getSongsInPlaylistsService implemented

--- src/services/postgres/PlaylistSongsService.js
+++ src/services/postgres/PlaylistSongsService.js
@@ -1,6 +1,7 @@
 const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
 const InvariantError = require("../../exceptions/InvariantError");
+const NotFoundError = require("../../exceptions/NotFoundError");
 
 class PlaylistSongsService {
     constructor () {
@@ -22,10 +23,46 @@ class PlaylistSongsService {
 
         if (!result.rowCount) {
             throw new InvariantError ('Gagal menambahkan lagu ke dalam playlist')
-        }
+        };
 
         return result.rows[0].id;
     }
-}
+
+    async getSongsByPlaylistId( playlistId ) {
+
+        const playlistQuery = {
+            text: `SELECT playlists.*, users.username FROM playlists LEFT JOIN users ON users.id = playlists.owner WHERE playlists.id = $1`,
+            values: [playlistId]
+        };
+
+        const playlistQueryResult = await this._pool.query(playlistQuery);
+
+        if (!playlistQueryResult.rowCount) {
+            throw new NotFoundError('Playlist tidak ditemukan');
+        };
+
+        const playlist = playlistQueryResult.rows[0];
+
+        const songsQuery = {
+            text: 'SELECT songs.id, songs.title, songs.performer FROM playlists JOIN playlist_songs ON playlist_songs.playlist_id = playlists.id JOIN songs ON songs.id = playlist_songs.song_id WHERE playlists.id = $1',
+            values: [playlistId]
+        };
+
+        const songsQueryResult = await this._pool.query(songsQuery);
+
+        if (!songsQueryResult.rowCount) {
+            throw new NotFoundError('Lagu tidak ditemukan');
+        };
+
+        const songs = songsQueryResult.rows;
+
+        const playlistSongsResult = {
+            ...playlist,
+            songs,
+        };
+
+        return playlistSongsResult;
+    };
+};
 
 module.exports = PlaylistSongsService;
